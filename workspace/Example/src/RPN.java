75ca3b7fe0845e30fb7206c1cdfa9e21f5447f83
Ashley-Marie Chin <china3@wit.edu>
2015-01-30 14:52:02 -0500

    Lab 1 done!

--- workspace/Example/src/RPN.java
+++ workspace/Example/src/RPN.java
@@ -1,30 +1,92 @@
-import java.io.StringReader;
 import java.util.Scanner;
 import java.util.Stack;
 
-
 public class RPN {
-	Stack<Integer> stack = new Stack<Integer>();
 	
-	Stack<Integer> evaluate(String expression) {
-		StringReader sr = new StringReader(expression);
-		Scanner scanner = new Scanner(sr);
-		while (scanner.hasNext()) {
-			String token = scanner.next();
-			// Check for +, -, /, *
-			if (token.equals("+")) {
-				// do the appropriate thing
-				// pop 2 items off stack, and place result
-				// onto stack
-			}
-			// catch all case
-			// put the integer on the stack
-			Integer.parseInt(token);
+	
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(scanner.hasNext()) 
+		{
+			String line = scanner.nextLine();
+			System.out.println("Your answer = " + evaluate(line.split(" ")));
 		}
 		
-		return stack;
+		
+		scanner.close();
 	}
-	void clear() {
-		stack.clear();
+
+
+
+	public static int operation(String sign, Integer a, Integer b) 
+	{
+		if(sign.equals("+")) {
+			return a + b;
+		}
+		
+		else if(sign.equals("-")) {
+			return b - a;
+		}
+		
+		else if(sign.equals("*")) {
+			return a * b;
+		}
+		
+		else if(sign.equals("/")) {
+			return b / a;
+		} 
+		
+		else 
+		{
+			throw new IllegalArgumentException("Invalid Operator.");
+		}
+	}
+
+	
+	public static Integer evaluate(String[] tokens) 
+	{
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(String token : tokens) 
+		{
+			if (token.equals("+")) 
+			{
+				stack.push(operation(token, stack.pop(), stack.pop()));
+			}
+			
+			else if(token.equals("-"))
+			{
+				stack.push(-operation(token, stack.pop(), stack.pop()));
+			}
+			else if(token.equals("*"))
+			 {
+				
+				stack.push(operation(token, stack.pop(), stack.pop()));
+			}
+			else if(token.equals("/"))
+			 {
+				stack.push(operation(token, stack.pop(), stack.pop()));
+			}
+			
+			else {
+				stack.push(Integer.parseInt(token));
+			}
+			
+		}
+		
+		return stack.pop();
+	}
+
+
+
+	public Stack<Integer> evaluate(String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
+
+
+
+
+
